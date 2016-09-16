55a54ab4ecd36c174c4b50dd487952f61880ca07
SSAJJALA <SSAJJALA@madisoncollege.edu>
2016-09-16 14:46:53 -0500

    updated

---
+++ src/test/edu/matc/persistence/CdmrDaoTest.java
@@ -0,0 +1,36 @@
+package edu.matc.persistence;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by student on 9/16/16.
+ */
+public class CdmrDaoTest {
+    @Test
+    public void getAllCdmrs() throws Exception {
+
+    }
+
+    @Test
+    public void getCdmr() throws Exception {
+
+    }
+
+    @Test
+    public void addCdmr() throws Exception {
+
+    }
+
+    @Test
+    public void deleteCdmr() throws Exception {
+
+    }
+
+    @Test
+    public void updateCdmr() throws Exception {
+
+    }
+
+}
