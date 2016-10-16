fbc3968f57408304719169e3aaa6745d839749d2
SSAJJALA <SSAJJALA@madisoncollege.edu>
2016-10-16 17:27:52 -0500

    updated

---
+++ src/main/java/com/cdmr/webservices/Customer.java
@@ -0,0 +1,89 @@
+package com.cdmr.webservices;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.annotation.Generated;
+
+@Generated("com.robohorse.robopojogenerator")
+public class Customer{
+	@JsonProperty("zip")
+	private String zip;
+	@JsonProperty("city")
+	private String city;
+	@JsonProperty("phone")
+	private String phone;
+	@JsonProperty("custNum")
+	private int custNum;
+	@JsonProperty("state")
+	private String state;
+	@JsonProperty("add2")
+	private String add2;
+	@JsonProperty("custName")
+	private String custName;
+	@JsonProperty("add1")
+	private String add1;
+
+	public void setAdd2(String add2){
+		this.add2 = add2; 
+	}
+
+	public void setPhone(String phone){
+		this.phone = phone; 
+	}
+
+	public String getZip(){
+		return zip; 
+	}
+
+	public String getAdd2(){
+		return add2; 
+	}
+
+	public void setCustName(String custName){
+		this.custName = custName; 
+	}
+
+	public String getCustName(){
+		return custName; 
+	}
+
+	public String getCity(){
+		return city; 
+	}
+
+	public String getAdd1(){
+		return add1; 
+	}
+
+	public void setState(String state){
+		this.state = state; 
+	}
+
+	public void setAdd1(String add1){
+		this.add1 = add1; 
+	}
+
+	public int getCustNum(){
+		return custNum; 
+	}
+
+	public void setCity(String city){
+		this.city = city; 
+	}
+
+	public String getPhone(){
+		return phone; 
+	}
+
+	public String getState(){
+		return state; 
+	}
+
+	public void setCustNum(int custNum){
+		this.custNum = custNum; 
+	}
+
+	public void setZip(String zip){
+		this.zip = zip; 
+	}
+
+}
---
+++ src/main/java/com/cdmr/webservices/CustomerLookupConsumer.java
@@ -0,0 +1,30 @@
+package com.cdmr.webservices;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by student on 10/16/16.
+ */
+public class CustomerLookupConsumer {
+    private final Logger log = Logger.getLogger(this.getClass());
+
+
+    public Customer getCustomerApiJSON(int customerNo) throws Exception {
+        Client client = ClientBuilder.newClient();
+        WebTarget target =
+                client.target("http://localhost:8080//rest/customer/" + customerNo);
+        String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        Customer result = mapper.readValue(response, Customer.class);
+        log.info("customer number:" + result.getCustNum());
+        log.info("customer name:" + result.getCustName());
+        return result;
+    }
+}
