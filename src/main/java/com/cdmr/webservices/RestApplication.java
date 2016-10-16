09f619f6b3ce242c1f77c15cba62b90949654414
SSAJJALA <SSAJJALA@madisoncollege.edu>
2016-10-15 23:56:29 -0500

    updated

---
+++ src/main/java/com/cdmr/webservices/RestApplication.java
@@ -0,0 +1,23 @@
+package com.cdmr.webservices;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by student on 10/15/16.
+ */
+
+
+//Defines the base URI for all resource URIs.
+@ApplicationPath("/")
+public class RestApplication extends Application {
+    //The method returns a non-empty collection with classes, that must be included in the published JAX-RS application
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet h = new HashSet<Class<?>>();
+        h.add(CustomerLookup.class );
+        return h;
+    }
+}
