286bb3379f8603c205f93fcf3d160dd342c3c046
SSAJJALA <SSAJJALA@madisoncollege.edu>
2016-11-12 19:06:00 -0600

    updated

--- src/main/java/com/cdmr/controller/LogoutServlet.java
+++ src/main/java/com/cdmr/controller/LogoutServlet.java
@@ -27,6 +27,6 @@ public class LogoutServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getSession().invalidate();
-        response.sendRedirect("/login.jsp");
+        response.sendRedirect("/index.jsp");
     }
 }
