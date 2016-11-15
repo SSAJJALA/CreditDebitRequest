d315b64625ab161f566d7b62e4fb78ae759f298a
SSAJJALA <SSAJJALA@madisoncollege.edu>
2016-11-14 21:37:29 -0600

    updated

--- src/main/java/com/cdmr/webservices/CustomerLookupConsumer.java
+++ src/main/java/com/cdmr/webservices/CustomerLookupConsumer.java
@@ -7,18 +7,32 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Created by student on 10/16/16.
  */
 public class CustomerLookupConsumer {
     private final Logger log = Logger.getLogger(this.getClass());
+    private Properties properties;
 
 
     public Customer getCustomerApiJSON(int customerNo) throws Exception {
+        properties = new Properties();
+
+        try {
+            properties.load (this.getClass().getResourceAsStream("/cdmr.properties"));
+        } catch (IOException ioe) {
+            System.out.println("cdmr.loadProperties()...Cannot load the properties file");
+            ioe.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("cdmr.loadProperties()..." + e);
+            e.printStackTrace();
+        }
         Client client = ClientBuilder.newClient();
         WebTarget target =
-                client.target("http://localhost:8080/rest/customer/" + customerNo);
+                client.target(properties.getProperty("customerLookupURL") + customerNo);
         String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
 
         ObjectMapper mapper = new ObjectMapper();
