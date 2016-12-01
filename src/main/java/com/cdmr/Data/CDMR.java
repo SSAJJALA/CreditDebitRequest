d601506739f125a7a4bd16215d04077c6a74f59f
SSAJJALA <SSAJJALA@madisoncollege.edu>
2016-12-01 17:24:00 -0600

    updated

--- src/main/java/com/cdmr/Data/CDMR.java
+++ src/main/java/com/cdmr/Data/CDMR.java
@@ -4,7 +4,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 /**
- * Created by Siva Sajjala on 9/26/16.
+ * This class is to hold the CDMR Requisition data. It includes CDMR header and adjustment details
+ *
+ * @author  Siva Sajjala
+ * @version 1.0
+ * @since   2016-09-26
  */
 public class CDMR {
     private int requisitionID;
@@ -23,10 +27,17 @@ public class CDMR {
     private List<CDMRAdjustments>  adjustments;
 
 
-
+    /**
+     * No arg constructor
+     */
     public CDMR() {
     }
 
+    /**
+     * constructor with args requisitionID, customer, status, invHeader, salesRepID, salesRepName, adjAmnt, adjGross, adjGross, adjAllowance, adjCharges, adjTax, cdmrDate, type, adjustments
+     * @param requisitionID, customer, status, invHeader, salesRepID, salesRepName, adjAmnt, adjGross, adjGross, adjAllowance, adjCharges, adjTax, cdmrDate, type, adjustments
+     *
+     */
     public CDMR(int requisitionID, Customer customer, String status, InvoiceHeader invHeader, String salesRepID, String salesRepName, Double adjAmnt, Double adjGross, Double adjAllowance, Double adjCharges, Double adjTax, LocalDate cdmrDate, String type, List<CDMRAdjustments> adjustments) {
         this.requisitionID = requisitionID;
         this.customer = customer;
@@ -44,118 +55,234 @@ public class CDMR {
         this.adjustments = adjustments;
     }
 
+    /**
+     * get the requisitionID
+     * @return requisitionID
+     */
     public int getRequisitionID() {
         return requisitionID;
     }
 
+    /**
+     * set the requisitionID
+     * @param requisitionID requisition id
+     */
     public void setRequisitionID(int requisitionID) {
         this.requisitionID = requisitionID;
     }
 
+    /**
+     * get the customer details
+     * @return customer
+     */
     public Customer getCustomer() {
         return customer;
     }
 
+    /**
+     * set the customer details
+     * @param customer customer details
+     */
     public void setCustomer(Customer customer) {
         this.customer = customer;
     }
 
+    /**
+     * get the status
+     * @return status
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * set the status
+     * @param  status
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * get the invoice header
+     * @return invHeader
+     */
     public InvoiceHeader getInvHeader() {
         return invHeader;
     }
 
+    /**
+     * set the invoice header
+     * @param  invHeader invoice header info
+     */
     public void setInvHeader(InvoiceHeader invHeader) {
         this.invHeader = invHeader;
     }
 
+    /**
+     * get the sales rep (TM) name
+     * @return salesRepID
+     */
     public String getSalesRepID() {
         return salesRepID;
     }
 
+    /**
+     * set the sales rep (TM) name
+     * @param  salesRepID sales rep (TM) name
+     */
     public void setSalesRepID(String salesRepID) {
         this.salesRepID = salesRepID;
     }
 
+    /**
+     * get the sales rep (TM) name
+     * @return salesRepName
+     */
     public String getSalesRepName() {
         return salesRepName;
     }
 
+    /**
+     * set the sales rep (TM) name
+     * @param salesRepName sales rep (TM) name
+     */
     public void setSalesRepName(String salesRepName) {
         this.salesRepName = salesRepName;
     }
 
+    /**
+     * get the adj amount
+     * @return adjAmnt
+     */
     public Double getAdjAmnt() {
         return adjAmnt;
     }
 
+    /**
+     * set the adj amount
+     * @param adjAmnt adj amount
+     */
     public void setAdjAmnt(Double adjAmnt) {
         this.adjAmnt = adjAmnt;
     }
 
+    /**
+     * get the cdmr date
+     * @return cdmrDate
+     */
     public LocalDate getCdmrDate() {
         return cdmrDate;
     }
 
+    /**
+     * set the cdmr date
+     * @param cdmrDate
+     */
     public void setCdmrDate(LocalDate cdmrDate) {
         this.cdmrDate = cdmrDate;
     }
 
+    /**
+     * get the type
+     * @return type
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * set the type of requisition
+     * @param type of the requisition
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * get the list of adjustments
+     * @return adjustments
+     */
     public List<CDMRAdjustments> getAdjustments() {
         return adjustments;
     }
 
+    /**
+     * get the adjustment details
+      * @param adjustments
+     */
     public void setAdjustments(List<CDMRAdjustments> adjustments) {
         this.adjustments = adjustments;
     }
 
+    /**
+     * get the Adjusted gross
+     * @return adjGross
+     */
     public Double getAdjGross() {
         return adjGross;
     }
 
+    /**
+     * set the Adjusted gross
+     * @param adjGross
+     */
     public void setAdjGross(Double adjGross) {
         this.adjGross = adjGross;
     }
 
+    /**
+     * get the Adj allowance
+     * @return adjAllowance
+     */
     public Double getAdjAllowance() {
         return adjAllowance;
     }
 
+    /**
+     * set the Adj allowance
+     * @param adjAllowance
+     */
     public void setAdjAllowance(Double adjAllowance) {
         this.adjAllowance = adjAllowance;
     }
 
+    /**
+     * get the Adjustement charges
+     * @return adjCharges
+     */
     public Double getAdjCharges() {
         return adjCharges;
     }
 
+    /**
+     * set the Adjustement charges
+     * @param adjCharges
+     */
     public void setAdjCharges(Double adjCharges) {
         this.adjCharges = adjCharges;
     }
 
+    /**
+     * get the adjTax
+     * @return adjTax
+     */
     public Double getAdjTax() {
         return adjTax;
     }
 
+    /**
+     * set the adjTax
+     * @param adjTax
+     */
     public void setAdjTax(Double adjTax) {
         this.adjTax = adjTax;
     }
 
+    /**
+     * toString methods to display the cdmr information.
+     * @return String
+     */
     @Override
     public String toString() {
         return "CDMR{" +
--- src/main/java/com/cdmr/Data/CDMRAdjustments.java
+++ src/main/java/com/cdmr/Data/CDMRAdjustments.java
@@ -3,7 +3,11 @@ package com.cdmr.Data;
 import java.util.List;
 
 /**
- * Created by student on 9/26/16.
+ * This class is to hold the CDMR Adjustment details data.
+ *
+ * @author  Siva Sajjala
+ * @version 1.0
+ * @since   2016-09-26
  */
 public class CDMRAdjustments {
     private int itemNum;
@@ -21,10 +25,29 @@ public class CDMRAdjustments {
     private Double newInvLineTotal;
     private List<CDMRComment> comments;
 
-
+    /**
+     * No arg constructor
+     */
     public CDMRAdjustments() {
     }
 
+    /**
+     * Constructor with args
+     * @param itemNum item number
+     * @param itemDesc item desc
+     * @param reasonCode reason code
+     * @param originalPrice original price
+     * @param originalQty qty
+     * @param adjQty adjusted qty
+     * @param originalInvLineTotal original invoice amount
+     * @param creditDebitFlg credit/debit flag
+     * @param allowanceAdjAmnt allowance adjustment amnt
+     * @param chargeAdjAmnt charge adjustment amnt
+     * @param taxAdjAmnt adjusted tax amnt
+     * @param lineAdjAmnt line adjustment amnt
+     * @param newInvLineTotal new invoice total
+     * @param comments comments
+     */
     public CDMRAdjustments(int itemNum, String itemDesc, String reasonCode, Double originalPrice, int originalQty, int adjQty, Double originalInvLineTotal, String creditDebitFlg, Double allowanceAdjAmnt, Double chargeAdjAmnt, Double taxAdjAmnt, Double lineAdjAmnt, Double newInvLineTotal, List<CDMRComment> comments) {
         this.itemNum = itemNum;
         this.itemDesc = itemDesc;
@@ -42,94 +65,186 @@ public class CDMRAdjustments {
         this.comments = comments;
     }
 
+    /**
+     * get the item number
+     * @return itemNum
+     */
     public int getItemNum() {
         return itemNum;
     }
 
+    /**
+     * set the item number
+     * @param itemNum item
+     */
     public void setItemNum(int itemNum) {
         this.itemNum = itemNum;
     }
 
+    /**
+     * get the item description
+     * @return itemDesc item description
+     */
     public String getItemDesc() {
         return itemDesc;
     }
 
+    /**
+     * set the item description
+     * @param itemDesc itemDesc item description
+     */
     public void setItemDesc(String itemDesc) {
         this.itemDesc = itemDesc;
     }
 
+    /**
+     * get the reason code
+     * @return reasonCode
+     */
     public String getReasonCode() {
         return reasonCode;
     }
 
+    /**
+     * set the reason code
+     * @param reasonCode reason code
+     */
     public void setReasonCode(String reasonCode) {
         this.reasonCode = reasonCode;
     }
 
+    /**
+     * get the original price
+     * @return originalPrice original price
+     */
     public Double getOriginalPrice() {
         return originalPrice;
     }
 
+    /**
+     * set the original price
+     * @param originalPrice original price
+     */
     public void setOriginalPrice(Double originalPrice) {
         this.originalPrice = originalPrice;
     }
 
+    /**
+     * get original qty
+     * @return originalQty original qty
+     */
     public int getOriginalQty() {
         return originalQty;
     }
 
+    /**
+     * set original qty
+     * @param  originalQty original qty
+     */
     public void setOriginalQty(int originalQty) {
         this.originalQty = originalQty;
     }
 
+    /**
+     * get the adjusted qty
+     * @return adjQty adj qty
+     */
     public int getAdjQty() {
         return adjQty;
     }
 
+    /**
+     * set the adjusted qty
+     * @param  adjQty adj qty
+     */
     public void setAdjQty(int adjQty) {
         this.adjQty = adjQty;
     }
 
+    /**
+     * get the original line total of the invoice
+     * @return originalInvLineTotal original line total of the invoice
+     */
     public Double getOriginalInvLineTotal() {
         return originalInvLineTotal;
     }
 
+    /**
+     * set the original line total of the invoice
+     * @param originalInvLineTotal original line total of the invoice
+     */
     public void setOriginalInvLineTotal(Double originalInvLineTotal) {
         this.originalInvLineTotal = originalInvLineTotal;
     }
 
+    /**
+     * get the credit/debit flag
+     * @return creditDebitFlg credit/debit flag
+     */
     public String getCreditDebitFlg() {
         return creditDebitFlg;
     }
 
+    /**
+     * set the credit/debit flag
+     * @param creditDebitFlg credit/debit flag
+     */
     public void setCreditDebitFlg(String creditDebitFlg) {
         this.creditDebitFlg = creditDebitFlg;
     }
 
+    /**
+     * get the line adjusted amount
+     * @return lineAdjAmnt Line adjusted amount
+     */
     public Double getLineAdjAmnt() {
         return lineAdjAmnt;
     }
 
+    /**
+     * set the line adjusted amount
+     * @param lineAdjAmnt line adjusted amount
+     */
     public void setLineAdjAmnt(Double lineAdjAmnt) {
         this.lineAdjAmnt = lineAdjAmnt;
     }
 
+    /**
+     * get the new invoice line total
+     * @return newInvLineTotal new invoice line total
+     */
     public Double getNewInvLineTotal() {
         return newInvLineTotal;
     }
 
+    /**
+     * set the new invoice line total
+     * @param newInvLineTotal new invoice line total
+     */
     public void setNewInvLineTotal(Double newInvLineTotal) {
         this.newInvLineTotal = newInvLineTotal;
     }
 
+    /**
+     * get the allowalce amount
+      * @return allowanceAdjAmnt allowalce amount
+     */
     public Double getAllowanceAdjAmnt() {
         return allowanceAdjAmnt;
     }
 
+    /**
+     * set the allowalce amount
+     * @param allowanceAdjAmnt allowalce amount
+     */
     public void setAllowanceAdjAmnt(Double allowanceAdjAmnt) {
         this.allowanceAdjAmnt = allowanceAdjAmnt;
     }
 
+    /**
+     * get the charge amount
+     * @return chargeAdjAmnt charge amount
+     */
     public Double getChargeAdjAmnt() {
         return chargeAdjAmnt;
     }
