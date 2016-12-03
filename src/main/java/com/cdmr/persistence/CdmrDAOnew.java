1a9e9459d0a4bcc942950718a56e984a158726f2
SSAJJALA <SSAJJALA@madisoncollege.edu>
2016-12-03 01:21:16 -0600

    Updated CDMR properties, cleaned up the code and added additional utility classes

---
+++ src/main/java/com/cdmr/persistence/CdmrDAOnew.java
@@ -0,0 +1,156 @@
+package com.cdmr.persistence;
+
+import com.cdmr.Data.CDMR;
+import com.cdmr.entity.Cdmr;
+import com.cdmr.util.AddRestrictions;
+import com.cdmr.util.ConvertToLocalDate;
+import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This is abstract implementation for CDMR table DAO. Fetches from the DB table CDMR.
+ *
+ * @author  Siva Sajjala
+ * @version 1.0
+ * @since   2016-12-03
+ */
+public class CdmrDAOnew extends GenericDAO {
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    /**
+     * get session
+     * @return session
+     */
+    @Override
+    protected Session getSession() {
+        return SessionFactoryProvider.getSessionFactory().openSession();
+    }
+
+    /**
+     * get all CDMRs
+     * @return List of cdmrs
+     * @throws HibernateException
+     */
+    @Override
+    public List<?> getAll() throws HibernateException {
+        List<Cdmr> cdmrs = new ArrayList<Cdmr>();
+        Session session = getSession();
+        cdmrs = session.createCriteria(Cdmr.class).list();
+        session.close();
+        return cdmrs;
+    }
+
+    /**
+     * get CDMR
+     * @param reqID
+     * @return CDMR object
+     * @throws HibernateException
+     */
+    @Override
+    public Object getOne(int reqID) throws HibernateException {
+        Session session = getSession();
+        Cdmr  cdmr = (Cdmr) session.get(Cdmr.class, reqID);
+        session.close();
+        return cdmr;
+
+    }
+
+    /**
+     * Add a CDMR
+     * @param obj
+     * @return Requisition ID
+     * @throws HibernateException
+     */
+    @Override
+    public int add(Object obj) throws HibernateException {
+        Session session = getSession();
+        Transaction tx = session.beginTransaction();
+        Cdmr cdmr = (Cdmr) obj;
+        session.save(cdmr);
+        tx.commit();
+        session.close();
+        int reqID = cdmr.getRequisitionID();
+        return reqID;
+
+    }
+
+    /**
+     * Delete a CDMR
+     * @param reqID
+     * @throws HibernateException
+     */
+    @Override
+    public void delete(int reqID) throws HibernateException {
+        Session session = getSession();
+        Transaction tx = session.beginTransaction();
+        Cdmr cdmr = (Cdmr) session.load(Cdmr.class,reqID);
+        log.info("CDMR:" + cdmr.toString());
+        session.delete(cdmr);
+        tx.commit();
+        session.close();
+        log.info("CDMR" + reqID + "deleted.");
+    }
+
+    /**
+     * Update a CDMR
+     * @param obj CDMR
+     * @throws HibernateException
+     */
+    @Override
+    public void update(Object obj) throws HibernateException {
+        Session session = getSession();
+        Transaction tx = session.beginTransaction();
+        Cdmr cdmr = (Cdmr) obj;
+        session.update(cdmr);
+        tx.commit();
+        session.close();
+    }
+
+    /**
+     * Search with filters
+     * @param searchOption
+     * @param operand
+     * @param value
+     * @return List of CDMRs
+     * @throws HibernateException
+     */
+    @Override
+    public List<?> getWithFilters(String searchOption, String operand, String value) throws HibernateException {
+        Session session = getSession();
+        Criteria c = session.createCriteria(Cdmr.class);
+        AddRestrictions addRestrictions = new AddRestrictions();
+
+        Object searchValue = null;
+
+        if (searchOption.equals("requisitionID")) {
+            searchValue = Integer.parseInt(value);
+        } else if (searchOption.equals("cdmrDate")) {
+            searchValue = new ConvertToLocalDate().formatDate(value);
+        } else {
+            searchValue = value;
+        }
+
+        log.info("searchOption: " + searchOption);
+        log.info("operand: " + operand);
+        log.info("searchValue: " + searchValue);
+
+        if (!searchOption.equals("all")) {
+            c= addRestrictions.addRestrictions(c, searchOption, operand, searchValue);
+        }
+
+        List<Cdmr> cdmrs = c.list();
+        session.close();
+        log.info("cdmrs: " + cdmrs.toString());
+        return cdmrs;
+
+    }
+
+
+
+}
---
+++ src/main/java/com/cdmr/persistence/GenericDAO.java
@@ -0,0 +1,44 @@
+package com.cdmr.persistence;
+
+import com.cdmr.entity.Cdmr;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by student on 12/3/16.
+ */
+public abstract class GenericDAO {
+
+    abstract protected Session getSession() ;
+
+    public List<?> getAll() throws HibernateException {
+
+        return new ArrayList<Object>();
+    }
+
+    public Object getOne(int t) throws HibernateException {
+
+        return null;
+    }
+
+    public int add(Object obj) throws HibernateException {
+
+        return 0;
+    }
+
+    public void delete(int t) throws HibernateException {
+
+    }
+
+    public void update(Object obj) throws HibernateException {
+
+    }
+
+    public List<?> getWithFilters(String a, String b, String c) throws HibernateException {
+
+        return new ArrayList<Object>();
+    }
+}
