179e39237ae6f436092fae97a1af9e9c3d214a4a
SSAJJALA <SSAJJALA@madisoncollege.edu>
2016-12-04 17:10:57 -0600

    Updated CDMR properties, cleaned up the code and added additional utility classes

--- src/test/com/cdmr/persistence/RequisitionDaoTest.java
+++ src/test/com/cdmr/persistence/RequisitionDaoTest.java
@@ -1,6 +1,10 @@
 package com.cdmr.persistence;
 
+import com.cdmr.entity.Requisition;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -8,9 +12,17 @@ import static org.junit.Assert.*;
  * Created by student on 9/16/16.
  */
 public class RequisitionDaoTest {
+    RequisitionDao requisitionDao = null;
+
+
+    @Before
+    public void setUp() throws Exception {
+        requisitionDao = new RequisitionDao();
+    }
     @Test
     public void getAllRequisitions() throws Exception {
-
+        List<Requisition> reqs = requisitionDao.getAllRequisitions();
+        assertNotNull("unable to get all reqs", reqs);
     }
 
     @Test
